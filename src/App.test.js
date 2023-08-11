3538ce98bdeaa43a6a15bc0480597d7b3c66e13b
Trinh Van Son <sontrinh.tp@gmail.com>
2023-08-11 13:36:21 +0700

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import App, { getSum } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// Kiểm tra hàm getSum
+test('getSum function returns correct sum', () => {
+  expect(getSum(1, 2)).toBe(3)
+  expect(getSum(-1, 2)).toBe(1)
+  expect(getSum(0, 0)).toBe(0)
+})
+
+// Kiểm tra các component có được render không
+test('renders input and button elements', () => {
+  const { getByTestId } = render(<App />)
+
+  fireEvent.change(getByTestId('value-one-input'), { target: { value: '1' } })
+  fireEvent.change(getByTestId('value-two-input'), { target: { value: '2' } })
+  fireEvent.click(getByTestId('button-add'));
+
+  expect(getByTestId('result')).toHaveTextContent('Result: 3')
+})
